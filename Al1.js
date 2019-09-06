417fdf3c85d300e3e85473a8968ea0846e57fcc5
HeribertoFigueroaMichel on Dcomplab <hfigueroa@ucol.mx>
2019-09-06 18:35:48 -0500

    first commit

---
+++ Al1.js
@@ -0,0 +1,13 @@
+//parte 1
+
+let sumador = 0
+for(let i = 1; i <=10; i++){
+   
+    sumador += i
+}
+
+console.log(`>${sumador}`)
+
+//parte 2
+let formula = 10*(10 + 1)/2
+console.log(`formula ${formula}`) 
